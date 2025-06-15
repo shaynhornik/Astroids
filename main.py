59533eaab773d4df663405f13038dd39b505d6ba
shaynhornik <shaynhornik@gmail.com>
2025-06-15 09:27:09 -0400

    player can now rotate and move around the screen

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +16,10 @@ def main():
                 if event.type == pygame.QUIT:
                             return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = (clock.tick(60))/1000
     
 
 if __name__ == "__main__":
